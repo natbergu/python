0fd0e483c618fdc3da3c9a7054602a48b9ad1705
natbergu <ncwb2048@gmail.com>
2020-09-15 21:47:27 -0700

    added better, funnier validation

--- Base Converters/bintodec.py
+++ Base Converters/bintodec.py
@@ -1,4 +1,22 @@
-num = input("Enter an unsigned binary (base 2) number: ")
+while True:
+    no = False
+    try:
+        num = int(input("Enter an unsigned binary (base 2) number: "))
+    except ValueError:
+        print("no.")
+        continue
+    if num < 0:
+        print("no.")
+        continue
+    else:
+        for digit in str(num):
+            if int(digit) > 1 and no == False:
+                print("no.")
+                no = True
+        if no == False:
+            break
+        else:
+            continue
 bin = [int(x) for x in str(num)]
 dec = 0
 for idx, val in enumerate(bin):
--- Base Converters/dectobin.py
+++ Base Converters/dectobin.py
@@ -1,13 +1,21 @@
 import math
-num = int(input("enter a natural number in decimal (base 10): "))
-if num <= 0:
-    print("no.")
-else:
-    binl = 1 + math.floor(math.log(num,2))
-    bin = [0] * binl
-    for idx, val in enumerate(bin):
-        slotval = 2**(binl-1-idx)
-        if (num - slotval) >= 0:
-            num -= slotval
-            bin[idx] = 1
-    print("Equivalent number in binary (base 2): " + "".join(map(str, bin)))
+while True:
+    try:
+        num = int(input("enter a natural number in decimal (base 10): "))
+    except ValueError:
+        print("no.")
+        continue
+    if num <= 0:
+        print("no.")
+        continue
+    else:
+        break
+num = int(num)
+binl = 1 + math.floor(math.log(num,2))
+bin = [0] * binl
+for idx, val in enumerate(bin):
+    slotval = 2**(binl-1-idx)
+    if (num - slotval) >= 0:
+        num -= slotval
+        bin[idx] = 1
+print("Equivalent number in binary (base 2): " + "".join(map(str, bin)))
