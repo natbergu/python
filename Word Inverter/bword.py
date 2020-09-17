fdb5911e98c3a1e307ebc3e8a3b0b9578b838bf1
natbergu <ncwb2048@gmail.com>
2020-09-16 18:15:19 -0700

    added new bword to bword.py

--- Word Inverter/bword.py
+++ Word Inverter/bword.py
@@ -1,5 +1,5 @@
 def bword(word):
-    """returns the word(s), backward!!"""
+    """Returns the word(s), backward."""
     offset = len(str(word)) - 1
     backword = []
     while offset > -1:
@@ -8,12 +8,23 @@ def bword(word):
     return "".join(map(str, backword))
 
 def bword2(word):
-    """return the word(s), backward another (faster? shorter.) way!!"""
+    """Returns the word(s), backward.
+
+another (faster? shorter.) way!!"""
     backword2 = []
     for letter in range(1, (len(str(word)) + 1)):
         backword2.append(str(word)[-1 * letter])
     return "".join(map(str, backword2))
 
+def bword3(word):
+    """Returns the word(s), backward.
+
+well now that i know this way i look pretty stupid!!"""
+    result = list(word)
+    result.reverse()
+    return "".join(map(str, result))
+
 word = input("word(s) > ")
 print(bword(word))
 print(bword2(word))
+print(bword3(word))
